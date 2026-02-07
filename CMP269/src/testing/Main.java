80233cbdb56305b881cb3897a092d48a7d8b1560
brianad459 <brianadeloatch14@gmail.com>
2026-02-06 19:42:48 -0500

    Added my main method.

---
+++ CMP269/src/testing/Main.java
@@ -0,0 +1,25 @@
+package testing;
+
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Payable> paymentQueue = new ArrayList<>();
+		
+		CreditCards ccard = new CreditCards("Briana Deloatch", 0);
+		MealPlan mealplan = new MealPlan("Ashton", 400);
+		paymentQueue.add(ccard);
+		paymentQueue.add(mealplan);
+		
+		for (int i = 0; i < paymentQueue.size(); i++ ) {
+			Payable p = paymentQueue.get(i);
+			p.processPayment(50.0);
+		}
+		
+		System.out.println("Total Transactions: "+ PaymentMethod.totalTransactions);
+		
+	}
+
+}
